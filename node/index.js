a27721f4627d99d85a0324edf6df693e7e032647
AlexKolykhalov <ak8647@rambler.ru>
2024-10-18 13:04:03 +0300

    vercel-testing: Changed path of static files

--- node/index.js
+++ node/index.js
@@ -8,10 +8,10 @@ const app = express();
 const __dirname = resolve();
 const port = 3000;
 
-// app.use(express.static(resolve("public")));
+app.use(express.static(resolve("public")));
 // app.use(router);
 app.get("/", (req, res) => {
-    res.status(200).sendFile(resolve("node", "public", "pages", "index", "index.html"));
+    res.status(200).sendFile(resolve("node", "pages", "index", "index.html"));
 });
 
 app.listen(port, () => {
