e9c435aa6a4e9b1ff70284e2d2e773b2ac9cfaa9
AlexKolykhalov <ak8647@rambler.ru>
2024-10-21 08:11:32 +0300

    vercel-testing: Simplify the code

--- node/index.js
+++ node/index.js
@@ -4,21 +4,18 @@ import express from "express";
 import { resolve } from "path";
 import { router } from "./src/routes/index.route.js";
 
-const port = process.env.PORT;
+const port = 3000;
 const app = express();
 
 // const __dirname = resolve("node");
-// app.use(express.static(resolve("public")));
-
+app.use(express.static(resolve("public")));
+app.use(express.json());
 app.use(router);
 
 try {
-    // app.get("/", (req, res) => {
-    // 	res.status(200).sendFile(resolve(__dirname, "public", "pages", "index", "index.html")); 
-    // });
     app.listen(port, () => {
 	console.log(`Server is running on ${port}`);
-    });    
+    });
 } catch (error) {
     console.error(error);
 }
