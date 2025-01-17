8512bd4c555ea0131a58cc3f2981b229c8137a3a
AlexKolykhalov <ak8647@rambler.ru>
2025-01-17 11:04:59 +0300

    vercel-testing: Changed path of login.html

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -18,7 +18,7 @@ router.post("/api/signup", emailValidator, passwordValidator, AuthController.sig
 router.get("/api/refresh", refreshTokenValidator,             AuthController.refresh);
 router.get("/api/logout",                                     AuthController.logout);
 
-router.get("/login",  (_, res) => { res.sendFile("var/task/public/html/login.html"); });
+router.get("/login",  (_, res) => { res.sendFile("html/login.html", { root: "public" }); });
 // router.get("/login",  (_, res) => { res.sendFile("html/login.html",  { root: staticPath }); });
 router.get("/signup", (_, res) => { res.sendFile("html/signup.html", { root: staticPath }); });
 
