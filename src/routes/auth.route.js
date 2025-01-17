00f489986a6996b3240c1e5e1ee8cfe9c9c81869
AlexKolykhalov <ak8647@rambler.ru>
2025-01-17 10:36:04 +0300

    vercel-testing: Init commit for the new code

---
+++ src/routes/auth.route.js
@@ -0,0 +1,24 @@
+//@ts-check
+
+import path from "path";
+
+import { Router }                            from "express";
+import { AuthController }                    from "../controllers/auth.controller.js";
+import { emailValidator, passwordValidator } from "../validators/auth.validator.js";
+import { refreshTokenValidator }             from "../validators/token.validator.js";
+
+const staticPath = process.env.NODE_ENV === "development" ?
+      process.env.DEV_STATIC_FILES_PATH :
+      process.env.PROD_STATIC_FILES_PATH;
+
+const router = Router();
+
+router.post("/api/login",  emailValidator, passwordValidator, AuthController.login);
+router.post("/api/signup", emailValidator, passwordValidator, AuthController.signup);
+router.get("/api/refresh", refreshTokenValidator,             AuthController.refresh);
+router.get("/api/logout",                                     AuthController.logout);
+
+router.get("/login",  (_, res) => { res.sendFile("html/login.html",  { root: staticPath }); });
+router.get("/signup", (_, res) => { res.sendFile("html/signup.html", { root: staticPath }); });
+
+export default router;
