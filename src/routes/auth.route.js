9920a059195f418908a9185b4f03130b9a2bc5e8
AlexKolykhalov <ak8647@rambler.ru>
2025-01-21 13:27:04 +0300

    vercel-testing: Added static "public" in root

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -5,9 +5,9 @@ import { AuthController }                    from "../controllers/auth.controlle
 import { emailValidator, passwordValidator } from "../validators/auth.validator.js";
 import { refreshTokenValidator }             from "../validators/token.validator.js";
 
-const staticPath = process.env.NODE_ENV === "development" ?
-      process.env.DEV_STATIC_FILES_PATH :
-      process.env.PROD_STATIC_FILES_PATH;
+// const staticPath = process.env.NODE_ENV === "development" ?
+//       process.env.DEV_STATIC_FILES_PATH :
+//       process.env.PROD_STATIC_FILES_PATH;
 
 const router = Router();
 
@@ -16,7 +16,7 @@ router.post("/api/signup", emailValidator, passwordValidator, AuthController.sig
 router.get("/api/refresh", refreshTokenValidator,             AuthController.refresh);
 router.get("/api/logout",                                     AuthController.logout);
 
-router.get("/login",  (_, res) => { res.sendFile("html/login.html", { root: staticPath }); });
-router.get("/signup", (_, res) => { res.sendFile("html/signup.html", { root: staticPath }); });
+router.get("/login",  (_, res) => { res.sendFile("html/login.html", { root: "public" }); });
+router.get("/signup", (_, res) => { res.sendFile("html/signup.html", { root: "public" }); });
 
 export default router;
